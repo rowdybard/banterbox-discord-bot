43438c6eca2887a4aff2450b5c7be7a01f47fccb
rowdybard <46615706+rowdybard@users.noreply.github.com>
2025-08-17 11:37:52 -0400

    Update localAuth.ts

--- server/localAuth.ts
+++ server/localAuth.ts
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import bcrypt from "bcryptjs";
-import { storage } from "./storage";
+import { storage } from "./storage.js";
 import type { Express } from "express";
 
 export async function setupLocalAuth(app: Express) {
